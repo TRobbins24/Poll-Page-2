e80d56bd82881c595cedfd161a3ef16f03d1af0a
TRobbins24 <84456198+TRobbins24@users.noreply.github.com>
2022-03-31 09:26:13 -0400

    Add files via upload

---
+++ pollScript.js
@@ -0,0 +1,43 @@
+const options = document.querySelectorAll("label");
+for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", ()=>{
+    for (let j = 0; j < options.length; j++) {
+      if(options[j].classList.contains("selected")){
+        options[j].classList.remove("selected");
+      }
+    }
+    options[i].classList.add("selected");
+    for (let k = 0; k < options.length; k++) {
+      options[k].classList.add("selectall");
+    }
+    let forVal = options[i].getAttribute("for");
+    let selectInput = document.querySelector("#"+forVal);
+    let getAtt = selectInput.getAttribute("type");
+    if(getAtt == "checkbox"){
+      selectInput.setAttribute("type", "radio");
+    }else if(selectInput.checked == true){
+      options[i].classList.remove("selected");
+      selectInput.setAttribute("type", "checkbox");
+    }
+    let array = [];
+    for (let l = 0; l < options.length; l++) {
+      if(options[l].classList.contains("selected")){
+        array.push(l);
+      }
+    }
+    if(array.length == 0){
+      for (let m = 0; m < options.length; m++) {
+        options[m].removeAttribute("class");
+      }
+    }
+  });
+}
+
+function createProgressBar(){
+    var random = Math.floor(Math.random() * 99) + 1;
+    var container = Document.getElementByClass("progress");
+    var div = Document.createElement("div");
+    div.classList.add("progress");
+    div.style.width == random;
+}
+
